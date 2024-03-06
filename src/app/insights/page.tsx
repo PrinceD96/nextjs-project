c8a1370e7363dd22104cd8b4d59f1f516509e977
Daniel Morales <daniel.a.moralesalt@gmail.com>
2024-03-05 21:08:17 -0500

    feat: implement basic routes with tabs

--- src/app/insights/page.tsx
+++ src/app/insights/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Insights() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold">Home</h1>
+      <h1 className="text-6xl font-bold">Insights</h1>
     </main>
   );
 }
